6bc67a77911672e3de1a542d302284cd7e58f850
georgemoralis <giorgosmrls@gmail.com>
2025-12-24 13:24:55 +0200

    added a few more source files

---
+++ src/common/memory_patcher.h
@@ -0,0 +1,33 @@
+// SPDX-FileCopyrightText: Copyright 2024 shadPS4 Emulator Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include <string>
+#include <vector>
+#include "common/types.h"
+
+namespace MemoryPatcher {
+
+enum PatchMask : u8 {
+    None,
+    Mask,
+    Mask_Jump32,
+};
+
+struct PendingPatch {
+    std::string modName;
+    std::string address;
+    std::string value;
+    std::string target;
+    std::string size;
+    bool littleEndian = false;
+    PatchMask mask = PatchMask::None;
+    int maskOffset = 0;
+};
+
+std::string convertValueToHex(std::string type, std::string valueStr);
+
+std::vector<PendingPatch> readPatches(std::string gameSerial, std::string appVersion);
+
+} // namespace MemoryPatcher
---
+++ src/core/emulator_state.cpp
@@ -0,0 +1,34 @@
+// SPDX-FileCopyrightText: Copyright 2025 shadLauncher4 Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <common/path_util.h>
+#include "emulator_state.h"
+
+std::shared_ptr<EmulatorState> EmulatorState::s_instance = nullptr;
+std::mutex EmulatorState::s_mutex;
+
+EmulatorState::EmulatorState() {}
+
+EmulatorState::~EmulatorState() {}
+
+std::shared_ptr<EmulatorState> EmulatorState::GetInstance() {
+    std::lock_guard<std::mutex> lock(s_mutex);
+    if (!s_instance)
+        s_instance = std::make_shared<EmulatorState>();
+    return s_instance;
+}
+
+void EmulatorState::SetInstance(std::shared_ptr<EmulatorState> instance) {
+    std::lock_guard<std::mutex> lock(s_mutex);
+    s_instance = instance;
+}
+
+bool EmulatorState::IsGameRunning() const {
+    return m_running;
+}
+void EmulatorState::SetGameRunning(bool running) {
+    m_running = running;
+}
